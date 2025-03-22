d1a05e22ec91024e2ee2203445c8c9f547015da0
Yiyuan Li <yy@eyuan.me>
2025-03-23 00:19:49 +0800

    update

--- app/main.go
+++ app/main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	defer l.Close()
+
+	fmt.Println("Server Started")
 
 	conn, err := l.Accept()
 	if err != nil {
